ed2ce3850face1bdbaac4b26ab5fcee03c43a90b
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-01-27 10:29:57 -0800

    first frontend layout

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import GPTA from './GPTA'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <GPTA/>
+    </div>
   )
 }
 
---
+++ client/src/Components/GPTA.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function GPTA() {
+  return (
+<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Upload your file here
+        <br className="hidden lg:inline-block"/>and have it marked in seconds
+      </h1>
+      <p className="mb-8 leading-relaxed">Import your document and get accurate notes </p>
+          <div className="flex justify-center">
+<input placeholder="insert text here" type="text" className="inline-flex text-white bg-gray border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg"/>
+<br/>
+  <button className="inline-flex m-2 text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">Check grammar</button>
+      </div>
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600/adadad/070708&text=el+coche+es+no+bonito+y+a+mi+gustar+mucho+las+palomitas.+La+casa+ser+grande+y+mi+gato+muy+travieso" />
+          <p>List of all the mistakes in Spanish that your teaching assistant has found</p>
+          <ul>
+            <li>mistake</li>
+            <li>mistake</li>
+            <li>mistake</li>
+            <li>mistake</li>
+          </ul>
+    </div>
+  </div>
+</section>  )
+}
+
+export default GPTA
