1fa5ef3600ddae8eaed762bfe6bdc5eb77de05c5
KennyK22 <kennethkarter@gmail.com>
2023-01-31 17:21:36 -0600

    style changes

--- client/src/Components/About.tsx
+++ client/src/Components/About.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='w-full flex flex-col justify-center content-center'>
+      <button className='bg-[#cc2936] text-white rounded p-1  shadow-md shadow-cyan-500/50 mt-2 w-[30%]'>Hello</button>
+      <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h1>
+    </div>
   )
 }
 
