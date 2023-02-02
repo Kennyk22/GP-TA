f59931f239ca7d0c5e1c5a5bf06bb7a91bcea0f9
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-02-02 09:36:33 -0800

    styling

--- client/src/Components/Pricing.tsx
+++ client/src/Components/Pricing.tsx
@@ -8,8 +8,8 @@ function Pricing() {
     <div className="flex flex-col text-center w-full mb-20">
       <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Pricing</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">spend some money, save a lot of TIME</p>
-      <div className="flex mx-auto border-2 border-[#ebbab9] rounded overflow-hidden mt-6">
-        <button className="py-1 px-4 bg-[#ebbab9] text-black focus:outline-none">Monthly</button>
+      <div className="flex mx-auto border-2 border-[#a70805] rounded overflow-hidden mt-6">
+        <button className="py-1 px-4 bg-[#a70805] text-white focus:outline-none">Monthly</button>
         <button className="py-1 px-4 focus:outline-none">Annually</button>
       </div>
     </div>
@@ -34,7 +34,7 @@ function Pricing() {
               </svg>
             </span>Spelling errors
           </p>
-          <button className="flex items-center mt-auto text-black bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Stripegi
+          <button className="flex items-center mt-auto text-black bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Stripe
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
@@ -43,8 +43,8 @@ function Pricing() {
         </div>
       </div>
       <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
-        <div className="h-full p-6 rounded-lg border-2 border-[#ebbab9] flex flex-col relative overflow-hidden">
-          <span className="bg-[#ebbab9] text-black px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
+        <div className="h-full p-6 rounded-lg border-2 border-[#a70805] flex flex-col relative overflow-hidden">
+          <span className="bg-[#a70805] text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
           <h2 className="text-sm tracking-widest title-font mb-1 font-medium">PRO</h2>
           <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
             <span>$38</span>
@@ -78,7 +78,7 @@ function Pricing() {
               </svg>
             </span>Keep feedback in different folders and download to your computer
           </p>
-          <button className="flex items-center mt-auto text-black bg-[#ebbab9] border-0 py-2 px-4 w-full focus:outline-none hover:bg-pink-600 rounded">Stripe
+          <button className="flex items-center mt-auto text-white bg-[#a70805] border-0 py-2 px-4 w-full focus:outline-none hover:bg-pink-600 rounded">Stripe
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
