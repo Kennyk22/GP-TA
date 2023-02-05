a3d6bdd96916a1b7dbd29218a778707450f26e9d
KennyK22 <kennethkarter@gmail.com>
2023-02-04 21:41:02 -0600

    fixed: errors

--- server/build/Controllers/AssignmentControllers.js
+++ server/build/Controllers/AssignmentControllers.js
@@ -30,16 +30,16 @@ exports.default = {
             });
             const openai = new openai_1.OpenAIApi(configuration);
             //FIRST AI CALL
-            const aiResponse1 = yield openai.createCompletion((0, Helpers_1.aiProp)("for the following text identify any grammatical errors and wrap each error between asterisks:" + content));
+            const aiResponse1 = yield openai.createCompletion((0, Helpers_1.aiProp)("for the following text identify any grammatical errors and wrap each error in asterisks:" + content));
             const feedback1 = JSON.stringify(aiResponse1.data.choices[0].text);
             // SECOND AI CALL
             const aiResponse2 = yield openai.createCompletion((0, Helpers_1.aiProp)("provide a numbered list of grammatical errors in this text with a short explanation:" + content));
             const feedback2 = JSON.stringify(aiResponse2.data.choices[0].text);
             //THIRD AI CALL
-            const aiResponse3 = yield openai.createCompletion((0, Helpers_1.aiProp)("Based on this text" + content + "can you tell me specific things I could do to improve my Spanish mentioning 5 of the mistakes. Answer this as if you were the teacher writing a feedback conclusion for an essay"));
+            const aiResponse3 = yield openai.createCompletion((0, Helpers_1.aiProp)("tell me 5 general things I could do to improve this text with short examples from the text and explain like you are a teacher:" + content));
             const feedback3 = (JSON.stringify(aiResponse3.data.choices[0].text));
             //COMBINES AI CALLS WITH WITH REMOVABLE ELEMENT INBETWEEN
-            const feedback = feedback1.join(" ") + "-+-" + feedback2.join(" ") + "-+-" + feedback3;
+            const feedback = feedback1 + "-+-" + feedback2 + "-+-" + feedback3;
             console.log(feedback);
             //calls auth0 for usertoken and extracts email
             const userEmail = yield (0, Helpers_1.getAuth0Email)(ctx);
