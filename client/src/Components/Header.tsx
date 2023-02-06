e9240336307c9a91966c8efe473d2d813b11caf9
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-02-06 13:25:04 -0800

    stylings

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -12,20 +12,20 @@ function Header() {
 
   const {logout, isAuthenticated} = useAuth0()
   return (
-<header className="text-white body-font rounded drop-shadow-lg bg-white">
+<header className="text-white body-font w-full h-30 rounded drop-shadow-lg bg-gray-400">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-black mb-4 md:mb-0">
      <Link to="/"> <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-20 h-20 text-black p-1 bg-[#cc2936] rounded-full" viewBox="0 -2 13 19">
             <GiRainbowStar />
           </svg></Link>
-      <span className="ml-3 text-xl">GP-TA</span>
+      <span className="text-white text-xl font-bold py-2 px-4 hover:text-black cursor-pointer transition duration-200 ease-in-out">GP-TA</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-     <Link to="/about"><a className="mr-5 text-black hover:text-[#ebbab9]">About</a></Link>
-      <Link to="/pricing"><a className="mr-5 text-black hover:text-[#ebbab9]">Pricing</a></Link>
+     <Link to="/about"><a className="text-white font-bold py-2 px-4 rounded hover:bg-black transition duration-200 ease-in-out">About</a></Link>
+      <Link to="/pricing"><a className="text-white font-bold py-2 px-4 rounded hover:bg-black transition duration-200 ease-in-out mr-5">Pricing</a></Link>
 
     </nav>
-        {isAuthenticated ? <button onClick={() => logout() } className="inline-flex items-center bg-[#cc2936] text-white border-0 py-1 px-3 focus:outline-none hover:bg-[#ebbab9] hover:text-[#08415c] rounded text-base mt-4 md:mt-0">Log out
+        {isAuthenticated ? <button onClick={() => logout() } className="inline-flex items-center bg-[#cc2936] text-white border-0 py-1 px-3 focus:outline-none hover:bg-red-700 hover:text-white rounded text-base mt-4 md:mt-0">Log out
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
