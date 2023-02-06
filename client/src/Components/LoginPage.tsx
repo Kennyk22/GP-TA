9c6498f03a532d541e751e2c2e1c8ad7fbf713e3
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-02-06 15:23:54 -0800

    newLogin

--- client/src/Components/LoginPage.tsx
+++ client/src/Components/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import sample from '../images/loginPageSample.png'
+import sample from '../images/GoodLoginPage.png'
 
 function LoginPage() {
 
