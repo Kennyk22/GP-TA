389387401932231ed73ccc9c6b12779787a1253b
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-02-06 09:13:40 -0800

    css changes

--- client/src/Components/DropDownAssignment.tsx
+++ client/src/Components/DropDownAssignment.tsx
@@ -45,7 +45,7 @@ export default function DropDownAssignment({array, title, checkGrammar}: {array:
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div className=' mt-5 lg:mt-2'>
-        <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+        <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-offset-2 focus:ring-offset-gray-100">
           {dropState.selectedTitle ? dropState.selectedTitle : title}
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
@@ -66,7 +66,7 @@ export default function DropDownAssignment({array, title, checkGrammar}: {array:
               return <Menu.Item key={el.id}>
                 {({ active }) => (
                   <div className='flex flex-row justify-center content-center' >
-                  <button                    
+                  <button
                     type="button"
                     onClick={() => onSelect(el.id, el.title)}
                     className={classNames(
