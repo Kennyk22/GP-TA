d33398d51ee445e9163682df9a19f1c2f8752259
KennyK22 <kennethkarter@gmail.com>
2023-02-07 10:06:14 -0600

    test fix

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -4,6 +4,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/About/i);
+  const priceEl = screen.getByText(/Pricing/i)
   expect(linkElement).toBeInTheDocument();
+  expect(priceEl).toBeInTheDocument();
+  
 });
