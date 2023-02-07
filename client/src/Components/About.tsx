9f88b09b66ea58cd7f17fd8293568336715f46dc
KennyK22 <kennethkarter@gmail.com>
2023-02-06 18:19:35 -0600

    Small fixes

--- client/src/Components/About.tsx
+++ client/src/Components/About.tsx
@@ -5,14 +5,10 @@ import React from 'react'
   //              Darkest Blue  Bright Red    Fade Red    Blue          cyan
   return (
     <div className='w-full flex flex-col justify-center content-center'>
-      <button className='bg-[#cc2936] text-2xl font-bold Alegreya  rounded-xl text-black bg-opacity-80 p-1 shadow-md shadow-black mt-2 w-[10%] hover:bg-[#ebbab9]'>Students</button>
-
-      <button className="bg-[#cc2936] hover:bg-[#cc2936] text-black font-bold py-2 bg-opacity-90 px-4 rounded-md shadow-md m-3 w-[10%]">
-        Students
-      </button>
-
-      <h1 className='title-font font-bold text-3xl text-[#08415c] Hind'>TITLE OF THIS PAGE Goes Here</h1>
-      <p className='Montserrat'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <h1 className='title-font font-bold text-3xl text-[#08415c] Hind py-5 px-5'>About Us</h1>
+      <p className='Montserrat px-5'>We are a team of software developers and teachers who have had it with the traditional grading system. teachers are expected to take time of they're valuable days off too grade dozens sometimes event hundreds of papers.
+        We saw a great opportunity to help teachers with the emerging technology of AI, which is great at tasks relating to lanuage and grammar. Our app allows teachers to utilize the power of openAI's chatGPT to grade papaers mmore effeciently than ever before!
+      </p>
     </div>
   )
 }
